4f143005f603747de2d0d7e23e7075c64e31bf84
Anton Funk <af@dbv.eu>
2015-09-25 14:35:26 +0200

    Dashboard hinzu

---
+++ themes/admin/views/site/dashboard.php
@@ -0,0 +1,29 @@
+<h4>YIITask-CMS</h4>
+
+<div class="row">
+    <div class="col-sm-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">Dashboard</div>
+            <div class="panel-body">
+
+                Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.
+                Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.
+                Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.
+                Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.
+                Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.
+                Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.
+                Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.
+                Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.
+                Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.
+                Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.
+                Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.
+                Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.
+                Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.
+                Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.Lorem Ipsum sit amet.
+
+            </div>
+        </div>
+
+    </div>
+    </div>
+
